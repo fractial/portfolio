1e369c9b9536d48e93d3fbcc018efff3d700c2b6
Fractial <126729237+fractial@users.noreply.github.com>
2024-08-12 20:59:23 +0200

    Fix mobile resolution

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -28,10 +28,10 @@ export default function Grid({ ...props }: HTMLAttributes<HTMLDivElement>) {
   }, []);
   return (
     <div
-      className="gap-4 grid grid-cols-3 *:bg-[#0095D520] dark:*:bg-[#806EE320] mx-auto px-8 *:p-4 *:rounded-xl max-w-4xl"
+      className="gap-4 grid grid-cols-3 *:bg-[#0095D5]/20 dark:*:bg-[#806EE3]/20 mx-auto px-8 *:p-4 *:rounded-xl max-w-4xl"
       {...props}
     >
-      <div className="col-span-2">
+      <div className="col-span-3 md:col-span-2">
         <Image
           src={`https://github-readme-stats.vercel.app/api?username=${username}&text_color=fff&icon_color=fff&border_color=ffffff00&bg_color=ffffff00&theme=dark&hide_title=true&show_icons=true&line_height=18`}
           alt="userstats"
@@ -40,19 +40,19 @@ export default function Grid({ ...props }: HTMLAttributes<HTMLDivElement>) {
           className="w-full invert dark:invert-0"
         />
       </div>
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center col-span-3 md:col-span-1">
         <p>
           <span className="font-bold text-6xl">{user?.public_repos || 0}</span>{' '}
           public repositories
         </p>
       </div>
-      <div className="*:brightness-0 gap-4 grid grid-cols-2 *:p-4 *:w-full *:aspect-square *:grayscale *:invert-0 dark:*:invert">
+      <div className="*:brightness-0 gap-4 grid grid-cols-4 md:grid-cols-2 col-span-3 md:col-span-1 *:p-4 *:w-full *:aspect-square *:grayscale *:invert-0 dark:*:invert">
         <Image src="/nextjs.svg" alt="nextjs" width={50} height={50} />
         <Image src="/git.svg" alt="git" width={50} height={50} />
         <Image src="/rust.svg" alt="rust" width={50} height={50} />
         <Image src="/figma.svg" alt="figma" width={50} height={50} />
       </div>
-      <div className="col-span-2">
+      <div className="col-span-3 md:col-span-2">
         <Image
           src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${username}&layout=compact&text_color=fff&border_color=ffffff00&bg_color=ffffff00&theme=dark&hide_title=true&line_height=18`}
           alt="userstats"
